6b8c8accefdad318a6b97704aef474f7e3a5af56
1nbetw33n <a.ab@mail.de>
2021-05-30 09:59:31 +0200

    copyright added to project

--- src/main/JAVA_SRC/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering/_1NSIDE_STYX/HelloServlet.java
+++ src/main/JAVA_SRC/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering/_1NSIDE_STYX/HelloServlet.java
@@ -1,3 +1,16 @@
+/*
+ *  copyright (c) 2021 1nbetw33n Labs.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * 1nbetw33n Labs. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * 1nbetw33n Labs.
+ *
+ */
+
 package _1nbetw33n._1NSIDE_STYX.SoftwareEngineering._1NSIDE_STYX;
 
 import java.io.*;
@@ -22,6 +35,5 @@ public class HelloServlet extends HttpServlet {
         out.println("</body></html>");
     }
 
-    public void destroy() {
-    }
+    final public void destroy() {System.out.println("SERVER ANNIHILATED");}
 }

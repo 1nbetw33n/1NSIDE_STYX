0daebce9af92439de8305f5e6e394920437cf997
1nbetw33n <a.ab@mail.de>
2021-06-01 17:06:11 +0200

    Implemented all tests and made every test case fail.

--- src/main/JAVA JUNIT TESTS/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX6/NO3/PersonTest.java
+++ src/main/JAVA JUNIT TESTS/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX6/NO3/PersonTest.java
@@ -15,57 +15,78 @@ package _1nbetw33n._1NSIDE_STYX.SoftwareEngineering1.EX6.NO3;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
+    private Long counter;
+    private String input;
+    private Person unix;
+
     @BeforeEach
     void setUp() {
+        this.counter = 0L;
+        this.input  = "Frau Dr. Eva Müller, Hochschule Bonn-Rhein-Sieg, Fachbereich Informatik, Grantham-Allee 20, 53757 Sankt Augustin";
+        this.unix   = new Person(input);
     }
 
     @AfterEach
     void tearDown() {
+        this.counter = null;
+        this.input = null;
+        this.unix  = null;
     }
 
     @Test
     void getSalutation() {
+        assertEquals("Frau", this.unix.getSalutation(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getTitle() {
+        assertEquals("Dr.", this.unix.getTitle(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getFirstName() {
+        assertEquals("Eva", this.unix.getFirstName(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getLastName() {
+        assertEquals("Müller", this.unix.getLastName(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getOrganisation() {
+        assertEquals("Hochschule Bonn-Rhein-Sieg", this.unix.getOrganisation(), "thats not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getDepartment() {
+        assertEquals("Fachbereich Informatik", this.unix.getDepartment(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getStreet() {
+        assertEquals("Grantham-Allee", this.unix.getOrganisation(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getHouseNumber() {
+        assertEquals("20", this.unix.getHouseNumber(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getZIPCode() {
+        assertEquals("53757", this.unix.getZIPCode(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 
     @Test
     void getTown() {
+        assertEquals("Sankt Augustin", this.unix.getTown(), "that's not working like intended. try smarter! - " + ++this.counter);
     }
 }

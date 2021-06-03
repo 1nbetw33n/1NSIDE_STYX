4479f65d022703bce562f0f24376c2e8d9937e99
1nbetw33n <a.ab@mail.de>
2021-06-03 14:31:38 +0200

    Created Auto + Autoable. Created Kunde + Kundeable. Created ParkHaus + ParkHausable. Created unit test classes for each.

---
+++ src/main/JAVA JUNIT TESTS/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX7/KundeTest.java
@@ -0,0 +1,30 @@
+/*
+ *  copyright (c) 2021 1nbetw33n Labs.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * 1nbetw33n Labs. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * 1nbetw33n Labs.
+ *
+ */
+
+package _1nbetw33n._1NSIDE_STYX.SoftwareEngineering1.EX7;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KundeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}

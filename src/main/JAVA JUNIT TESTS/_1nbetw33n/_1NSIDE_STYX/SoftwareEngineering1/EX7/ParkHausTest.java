83059fc3772f0d3d7e591e94470ea03848e1b6e8
1nbetw33n <a.ab@mail.de>
2021-06-05 10:53:46 +0200

    Made both test cases work!:D

--- src/main/JAVA JUNIT TESTS/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX7/ParkHausTest.java
+++ src/main/JAVA JUNIT TESTS/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX7/ParkHausTest.java
@@ -79,9 +79,9 @@ final class ParkHausTest {
         this.kunde5.firma = true;
         this.parkhaus.gebühr = 13;
         this.autoListSum = Arrays.stream(this.autos)
-                                 .filter(x -> (x.hatTurbo() && x.kunde.istFirma()))
-                                 .map(x -> (x.parkdauer * parkhaus.gebühr()))
-                                 .reduce(0, Integer::sum);
+                                            .filter(x -> (x.hatTurbo() && x.kunde.istFirma()))
+                                            .map(x -> (x.parkdauer * parkhaus.gebühr()))
+                                            .reduce(0, Integer::sum);
         this.ListOfCarsParkingLongerThanOneDay = Arrays.stream(this.autos)
                                                                                     .filter(x -> (x.kunde.istFirma() && x.parkdauer() > 1440))
                                                                                     .collect(Collectors.toList());

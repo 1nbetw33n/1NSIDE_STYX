20965b8331249feae4662b4a87ce0283d41d28d8
1nbetw33n <a.ab@mail.de>
2021-06-11 14:50:02 +0200

    Created EvenUnevenIterator + EvenUnevenIteratorTest

---
+++ src/main/JAVA_SRC/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX8/NO1/EvenUnevenIterator.java
@@ -0,0 +1,40 @@
+/*
+ *  copyright (c) 2021 1nbetw33n Labs.
+ *  Planet Earth, Milky Way, Virgo Supercluster.
+ *  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * 1nbetw33n Labs. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * 1nbetw33n Labs.
+ *
+ */
+
+package _1nbetw33n._1NSIDE_STYX.SoftwareEngineering1.EX8.NO1;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class EvenUnevenIterator implements Iterator<List> {
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    public boolean hasNext() {
+        return false;
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    public List next() {
+        return null;
+    }
+}

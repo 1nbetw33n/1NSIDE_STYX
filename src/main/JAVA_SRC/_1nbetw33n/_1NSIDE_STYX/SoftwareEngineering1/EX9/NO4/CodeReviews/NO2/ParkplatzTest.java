cc2606d710cb4e2e00a97ae7d36f7983f7fa11bd
1nbetw33n <a.ab@mail.de>
2021-06-17 13:13:16 +0200

    Created a package for the second code review and added the code

--- src/main/JAVA_SRC/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX9/NO4/CodeReviews/NO2/ParkplatzTest.java
+++ src/main/JAVA_SRC/_1nbetw33n/_1NSIDE_STYX/SoftwareEngineering1/EX9/NO4/CodeReviews/NO2/ParkplatzTest.java
@@ -11,49 +11,32 @@ import org.junit.jupiter.api.Test;
 
 class ParkplatzTest {
 
-
     Parkplatz parkplatz;
-
     Auto auto;
-
 
     @BeforeEach
     void setup() {
-
         parkplatz = new Parkplatz(3);            // erstellen einen Parkplatz mit 3 freie Plätze
-
         auto = new Auto(parkplatz);
-
     }
 
 
     @Test
-
     @DisplayName("Prueft ob es noch Platz gibt")
     void isVollTest() {
-
         auto.parken();
-
         auto.parken();
-
         auto.parken();
-
         assertTrue(parkplatz.isVoll());
-
     }
 
 
     @Test
-
     @DisplayName("zeigt die verfuegbare Platz im Parkplatz")
     void freiplatzTest() {
-
         auto.parken();
-
         auto.parken();
-
         assertEquals(1, parkplatz.freiplatz());
-
     }
 
 
